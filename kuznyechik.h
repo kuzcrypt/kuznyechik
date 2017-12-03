2d8a701a670f6bd84462d094b7fe758636f13146
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-12-03 17:49:10 +0100

    defined API in kuznyechik.h

---
+++ kuznyechik.h
@@ -0,0 +1,22 @@
+#ifndef __KUZNYECHIK_H
+#define __KUZNYECHIK_H
+
+#include <stdint.h>
+
+struct kuznyechik_subkeys {
+	uint64_t ek[10][2];
+	uint64_t dk[10][2];
+};
+
+int kuznyechik_set_key(struct kuznyechik_subkeys *subkeys,
+		       const unsigned char *key);
+
+void kuznyechik_encrypt(struct kuznyechik_subkeys *subkeys, unsigned char *out,
+			const unsigned char *in);
+
+void kuznyechik_decrypt(struct kuznyechik_subkeys *subkeys, unsigned char *out,
+			const unsigned char *in);
+
+void kuznyechik_wipe(struct kuznyechik_subkeys *subkeys);
+
+#endif /* __KUZNYECHIK_H */
